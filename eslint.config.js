31822d224d3bb744cb17ec1c42a52c73ab16a386
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2024-07-05 14:07:59 -0300

    chore: add prettier integration with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic
+  ...tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
 );

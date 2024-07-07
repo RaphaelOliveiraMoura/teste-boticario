f3c5a80dbf88bfb3be0cd9dd0997c00723e40747
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2024-07-07 09:09:12 -0300

    feat: use roboto font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
 import { Toaster } from "@/ui/shadcn/toaster";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         {children}
         <Toaster />
       </body>

ef6c49892b0533637906a0ed822c1bc5d26402d9
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2024-07-08 07:01:04 -0300

    feat: add money & date formatters

---
+++ frontend/src/infra/services/formatters.ts
@@ -0,0 +1,10 @@
+export const formatCurrency = (value: number) => {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+};
+
+export const formatDate = (date: Date) => {
+  return date.toLocaleDateString("pt-BR");
+};
